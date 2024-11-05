ac00483d85a1a0eeabfa0378fa491e0d112aace9
amyyisdead <754989@student.dusd.net>
2024-11-05 11:42:26 -0800

    animation
    Today i made a animation for my enemy character and am working on a animation for my main character

--- 2DBegginerAdvertureP4Perez/Assets/Scripts/EnemyController.cs
+++ 2DBegginerAdvertureP4Perez/Assets/Scripts/EnemyController.cs
@@ -12,18 +12,21 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer = Time.deltaTime;
+        timer -= Time.deltaTime;
         if (timer < 0)
         {
             direction = -direction;
@@ -35,10 +38,14 @@ public class EnemyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
         if(vertical)
         {
+            animator.SetFloat("Move x", 0);
+            animator.SetFloat("Move y", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("Move x", direction);
+            animator.SetFloat("Move y", 0);
             position.x = position.x + Time.deltaTime * speed * direction;
         }
       

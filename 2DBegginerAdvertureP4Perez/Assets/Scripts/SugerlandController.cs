167a78fc51db70c5f9717865a7e24d9b6065199f
amyyisdead <754989@student.dusd.net>
2024-11-05 18:48:58 -0800

    animation
    i fixed my animation

--- 2DBegginerAdvertureP4Perez/Assets/Scripts/SugerlandController.cs
+++ 2DBegginerAdvertureP4Perez/Assets/Scripts/SugerlandController.cs
@@ -18,11 +18,16 @@ public class PLayerController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
       
 
     }
@@ -32,6 +37,17 @@ public class PLayerController : MonoBehaviour
     {
          horizontal = Input.GetAxis("Horizontal");
          vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        if(!Mathf.Approximately(move.x,0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look x", lookDirection.x);
+        animator.SetFloat("Look y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
 
         if(isInvincible)
         {
@@ -56,7 +72,10 @@ public class PLayerController : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        if(amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if(isInvincible)
             {
                 return;
